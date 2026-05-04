e941647fd8d7a90fb81fabd0ce53df5f439609b4
NicoAlmiron <almiron.nicolas16@gmail.com>
2026-05-04 20:27:31 -0300

    Se comenzo con el tp4

---
+++ TP_04/Ejercicio_03.py
@@ -0,0 +1,15 @@
+# 3. Tengo la siguiente función matemática:
+# f(x) =3x -2
+# Deberá escribir un programa que realice el cálculo de la función, teniendo en cuenta los
+# siguientes puntos:
+#  La  función deberá ingresar al menos 5 valores para X para obtener los resultados de
+# y=f(x)
+#  Los valores ingresados pueden ser enteros o números con dos decimales únicamente,
+# los cuales también pueden ser negativos.
+#  Los resultados de la función deben ser impresos por pantalla en el siguiente formato(es
+# un ejemplo a modo ilustrativo):
+# x y=f(x)
+# 1 4
+# 8 10 -5 -45
+# 2,23 4,87
+# 7 14
