deff7f55a10a60a41737923040eff0261b6b86ca
NicoAlmiron <almiron.nicolas16@gmail.com>
2026-05-22 16:27:36 -0300

    se realizo la funcion de buscar y se empezo la de prestamo en biblioteca

--- TP_05/Biblioteca/main.py
+++ TP_05/Biblioteca/main.py
@@ -12,8 +12,9 @@ def limpiar_consola():
 #         "stock": "",
 #      }
 
-libros = [
-]
+libros = []
+
+prestamos = []
 
 categorias = ("Programación", "Base de Datos", "Redes", "Electronica")
 
@@ -27,7 +28,7 @@ def agregar_libro():
     print('Categorias: ')
     for _ in range(len(categorias)):
             print(f"-({_}): {categorias[_]}")
-    categoria = categorias[int((input('categoria: '))) - 1]
+    categoria = categorias[int((input('categoria: ')))]
     stock = int(input('cantidad de existencias: '))
 
 
@@ -40,6 +41,7 @@ def agregar_libro():
     }
 
     libros.append(libro)
+    input('Precione cualquier tecla para continuar...')
 
 
 def mostrar_libros():
@@ -52,13 +54,64 @@ def mostrar_libros():
         print(f'#Autor: {libro['autor']}')
         print(f'#Categoria: {libro['categoria']}')
         print(f'#Stock: {libro['stock']}')
+    input('Precione cualquier tecla para continuar...')
 
+
+def buscar_libro():
+    print('-------|###| BIBLIOTECA |###|-------')
+    print('-------|#  Buscar libros   #|-------')
+    print('ingrese el titulo del libro')
+    tituloLibro = input('Titulo:  ')
+    
+    for libro in libros:
+        if libro.get('titulo') == tituloLibro:
+            print(f'--|#Codigo: {libro.get('codigo')}')
+            print(f'#Titulo: {libro.get('titulo')}')
+            print(f'#Autor: {libro.get('autor')}')
+            print(f'#Categoria: {libro.get('categoria')}')
+            print(f'#Stock: {libro.get('stock')}')
+        
+    input('Precione cualquier tecla para continuar...')
+    
+    
+def prestar_libro():
+    print('-------|###| BIBLIOTECA |###|-------')
+    print('-------|#Solicitar Prestamo#|-------')
+    while True:
+        libroAPrestar= input('--#Titulo del Libro: ')
+        for libro in libros:
+            if libro.get('titulo') == libroAPrestar:
+                print(f'--|#Codigo: {libro.get('codigo')}')
+                print(f'#Titulo: {libro.get('titulo')}')
+                print(f'#Autor: {libro.get('autor')}')
+                print(f'#Categoria: {libro.get('categoria')}')
+                print(f'#Stock: {libro.get('stock')}')
+                
+                confirmar = input('Confirmar este libro? (S/N):')
+                if confirmar == "S" :
+                    print('se confirmo')
+                    nombre = input('Nombre completo: ')
+                    fechaActual = input('Fecha Actual (dd-mm-aaaa): ')
+                    codigoLibro = libro['codigo']                    
+                    prestamo = {
+                        "nombre": nombre,
+                        "fechaActual": fechaActual,
+                        "fechaEntrega": "",
+                        "codigoLibro": codigoLibro,
+                        "entregado": False
+                    }
+                    
+                    prestamos.append(prestamo)
+            else:
+                print('No se encontro el libro')
+        
 
 while True:
+    limpiar_consola()
     print('-------|###| BIBLIOTECA |###|-------')
     print('-( 1 )- - - -> Agregar Libro')
     print('-( 2 )- - - -> Mostrar Libros')
-    print('-( 3 )- - - -> X')
+    print('-( 3 )- - - -> Buscar Libros')
     print('-( 4 )- - - -> X')
     print('-( 5 )- - - -> X')
     print('-( 6 )- - - -> X')
@@ -73,7 +126,7 @@ while True:
     elif opcion == 2:
         mostrar_libros()
     elif opcion == 3:
-        print('-( 3 )- - -> Buscar Libro')
+        buscar_libro()
     elif opcion == 4:
         print('-( 4 )- - -> Prestar Libro')
     elif opcion == 5:
