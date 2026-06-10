14faf731f2c7e79d48be9f4a391e70bfc81e579b
NicoAlmiron <almiron.nicolas16@gmail.com>
2026-06-10 20:57:09 -0300

    se completo todo el tp de mejoras

--- TP_05/Stock/main.py
+++ TP_05/Stock/main.py
@@ -4,7 +4,7 @@ def limpiar_consola():
     os.system('cls' if os.name == 'nt' else 'clear')
 #lo anterior se realiza para poder limpiar la consola
 
-producto = {}
+
 productos = [
     {
         "id": 1,
@@ -17,26 +17,69 @@ productos = [
         "nombre": "mouse gamer RGB",
         "precio": 50000,
         "stock": 9
+    },
+    {
+        "id": 3,
+        "nombre": "prueba",
+        "precio": 90000,
+        "stock": 10
     }
 ]
 
 
+# ventas = [
+#     {
+#         "id_venta": 1,
+#         "detalle_venta": {
+#             1 : (2, 300000),
+#             2 : (1, 50000)
+#         },
+#         "importe_total": 350000,
+#         "forma_pago": 'Efectivo'
+#     },
+#     {
+#         "id_venta": 2,
+#         "detalle_venta": {
+#             2 : (1, 50000)
+#         },
+#         "importe_total": 50000,
+#         "forma_pago": 'Tarjeta'
+#     },
+# ]
+
 ventas = [
     {
         "id_venta": 1,
-        "detalle_venta": {
-            1 : (2, 300000),
-            2 : (1, 50000)
+        "detalle_venta": [
+        {
+            'id_producto' : 1,
+            'detalle_prod' : 
+            {
+                'cantidad': 2,
+                'sub_total': 300000       
+            },
         },
+        {
+            'id_producto' : 2,
+            'detalle_prod' :
+            {
+                'cantidad': 1,
+                'sub_total': 50000                
+            },
+        }],
         "importe_total": 350000,
-        "forma_pago": 'Efectivo'
+        "forma_pago": 'Efectivo',
     },
     {
         "id_venta": 2,
-        "detalle_venta": {
-            
-            2 : (1, 50000)
-        },
+        "detalle_venta": [{
+            'id_producto': 1,
+            'detalle_prod':
+            {
+                   'cantidad': 1,
+                   'sub_total': 50  
+            }              
+           }],
         "importe_total": 50000,
         "forma_pago": 'Tarjeta'
     },
@@ -45,6 +88,7 @@ ventas = [
 metodos_pago = ("Efectivo", "Transferencia", "Tarjeta")
 
 def agregar_prod():
+    producto = {}
     print('|###--··  TecStore  ··--###|')
     print('|##-· Producto Nuevo')
     producto['id'] = len(productos)+1
@@ -58,6 +102,7 @@ def agregar_prod():
 
 
 def mod_prod():
+    producto = {}
     print('|###--··  TecStore  ··--###|')
     print('|##-· Ingrese el Id del producto')
     id_buscar = input('|##-· ID: ')
@@ -147,35 +192,50 @@ def mostrar_un_prod(id):
             return producto
     if encontrado is False : return False
 
+def descontar_stock(id,cantidad):
+    for producto in productos:
+        for clave, valor in producto.items():
+            if clave == 'id' and valor == id:
+                producto['stock'] = producto['stock'] - cantidad
+
 
 def realizar_venta():
+    descuento = False
     venta = {}
-    list_venta = {}
+    list_venta = []
     print('|###--··  TecStore  ··--###|')
     listar_productos()
     total_venta = 0
-    print('|##-· Ingrese el Id del producto o X para continuar')
     while True:
+        print('|##-· Ingrese el Id del producto o Dejar vacio para continuar')
         id_v = input('|##-· ID: ')
-        if id_v.lower() == 'x':
+        if id_v.lower() == '':
             break
         for producto in productos:
             if int(id_v) == producto['id']:
+                cant = 1
                 cant = int(input('|##-· Cantidad: '))
                 if producto['stock'] > 0:
-                    v = (cant , (producto['precio'] * cant))
+                    v = {
+                            'cantidad': cant,
+                            'sub_total': (producto['precio'] * cant)  
+                        }
                     total_venta = total_venta + (producto['precio'] * cant)
-                    producto['stock'] = producto['stock'] - cant
-                    list_venta.update({producto['id'] : v})
+                    #producto['stock'] = producto['stock'] - cant
+                    prod_venta = {
+                        'id_producto': producto['id'],
+                        'detalle_prod': v
+                    }
+                    list_venta.append(prod_venta)
                 else:
                     print('|# Producto Sin Stock')
-                print(f'|#-{producto['nombre']} Agregado a la compra')        
+                print(f'|#-{producto['nombre']} x {cant} Agregado a la compra')        
     venta["importe_total"] = total_venta
     if len(list_venta) > 0:
         venta['id_venta'] = len(ventas) + 1
         venta['detalle_venta'] = list_venta
         while True:
-            print('|##-· Ingrese la forma de pago para continuar')
+            print('|##-· Para continuar, ingrese la forma de pago')
             print('|· (1) Efectivo · (2) Transferencia · (3) Tarjeta · (0) Cancelar')
             op =  input('|##-Forma de Pago: ')
             match op:
@@ -198,15 +258,21 @@ def realizar_venta():
             print('|##-· Aplicar descuentos')
             print('|· (1) % Porcentaje · (2) $ Importe · (0) Sin Descuentos')
             opc = input('|##-Opcion: ')
-            desc = float(input('|#  ·Descuento: '))
+            
 
             if opc == "1":
-                venta['importe_total'] = float(venta['importe_total'])*(float(venta['importe_total'])*(desc/100))
-                print(f'|-----------Importe Actualizado ${venta['importe_total']}')
+                desc = float(input('|#  ·Descuento: %'))
+                venta['importe_total'] = float(venta['importe_total'])-(float(venta['importe_total'])*(desc/100))
+                print(f'|-----------Importe Total ${total_venta} ')
+                print(f'|-----------Importe con Descuento ${venta['importe_total']} ')
+                descuento = True
                 break
             elif opc == "2":
+                desc = float(input('|#  ·Descuento: $'))
                 venta['importe_total'] = venta['importe_total'] - desc
-                print(f'|-----------Importe Actualizado ${venta['importe_total']}')
+                print(f'|-----------Importe Total ${total_venta} ')
+                print(f'|-----------Importe con Descuento ${venta['importe_total']} ')
+                descuento = True
                 break
             elif opc == "0":
                 print(f'|-----------Importe Actual ${venta['importe_total']}')
@@ -215,17 +281,24 @@ def realizar_venta():
                 print(f'|# No se indico una opcion valida!')
 
         
+        for prod_v in list_venta:
+            descontar_stock(prod_v['id_producto'],prod_v['detalle_prod']['cantidad'])
         ventas.append(venta)
-        print("Se realizo la venta exitosamente!")
-        print('|# Resumen')
+
+        print("|##·· Se realizo la venta exitosamente! ··##|")
+        print('|# Resumen de la venta!')
         print('|- Productos')
         print('|········································')
-        for prod, detalle in venta['detalle_venta'].items():
-            mostrar_un_prod(prod)
-            print(f'|\t\tx {detalle[0]} - SubTotal ${detalle[1]} ')
+        for prodc_v in venta['detalle_venta']:
+            producto = mostrar_un_prod(prodc_v['id_producto'])
+            print(f'|{producto['nombre']} - precio unitario ${producto['precio']} ')
+            print(f'|\t\tx {prodc_v['detalle_prod']['cantidad']} - SubTotal ${prodc_v['detalle_prod']['sub_total']} ')
             print('|········································')
-        print(f'|- Total: ${venta['importe_total']}')
+        print(f'|- Total: ${total_venta}')
+        if descuento : print(f'|- Total(Descuento): ${venta['importe_total']}')
         print(f'|- Forma de pago: {venta['forma_pago']}')
+
+
         
 def listar_ventas():
     print('|# Lista de Ventas')
@@ -233,15 +306,19 @@ def listar_ventas():
         print('|  ·No se emcontraron ventas!')
         return
     for venta in ventas:
+        print(venta)
         print(f'|- Venta Nroº: {venta['id_venta']}')
         print('|- Productos')
-        for prod, detalle in venta['detalle_venta'].items():
+        for prod_vent in venta['detalle_venta']:
             for producto in productos:
-                if producto['id'] == prod:
-                    print(f'|\t-{producto['nombre']} | ${producto['precio']} - x {detalle[0]} - SubTotal ${detalle[1]} ')
+                if producto['id'] == prod_vent['id_producto']:
+                    print(f'|\t-{producto['nombre']} | ${producto['precio']} - x {prod_vent['detalle_prod']['cantidad']} - SubTotal ${prod_vent['detalle_prod']['sub_total']} ')
         print(f'|- Total: ${venta['importe_total']}')
         print(f'|- Forma de pago: {venta['forma_pago']}')
         print('|········································')
+
+
+
 
 def fitrador(filtro, buscar):
     encontrado = False
@@ -327,52 +404,107 @@ def estadisticas_ventas():
     cant_ventas = len(ventas)
     cant_ganancias = 0
 
+    ganancias_totales = 0
+    #ganancias_netas = 0
+
     metodos_contados = {
         # 'efectivo':{
-        #  cantidad_de_usos : importe_total   
+        #  'veces_usado' : 4
+        #  'importe_metodo' : 70000
         # }
     }
 
     prod_vendidos = {
-        # id_prod : cantidad_vendidos
+        # 3 : {
+        #     # 'nombre' : 'prueba',
+        #     # 'cantidad_vendidos': 5
+        # }
     }
     
 
+    # {
+#         "id_venta": 2,
+#         "detalle_venta": [{
+#             'id_producto': 1,
+#             'detalle_prod':
+#             {
+#                    'cantidad': 1,
+#                    'sub_total': 50  
+#             }              
+#            }],
+#         "importe_total": 50000,
+#         "forma_pago": 'Tarjeta'
+#     },
+
     for venta in ventas:
         cant_ganancias = cant_ganancias + float(venta['importe_total'])
-        for clave, prod in venta.items():
-            if clave == 'detalle_venta':
-                for id_prod, detalles in prod.items():
-                    cant_prod_v = 0
-                    if id_prod in prod_vendidos:
-                        cant_prod_v = int(prod_vendidos[id_prod]) + detalles[0]
-                    else:
-                        cant_prod_v = detalles[0]
-                    
-                    prod_vendidos.update({id_prod: cant_prod_v})
+        for prod_det_vent in venta['detalle_venta']:
+            p = {}
+            nombre = ""
+            cant_prod_v = 0
+        
+        ganancias_totales = + ganancias_totales
+
+        for clave, valor in prod_vendidos.items():
+            if clave == 'id_prod' and prod_det_vent['id_producto'] == valor:
+                cant_prod_v = int(prod_vendidos['cantidad_vendidos']) + int(prod_det_vent['detalle_prod']['cantidad'])
+
+        if cant_prod_v == 0:
+            cant_prod_v = int(prod_det_vent['detalle_prod']['cantidad'])
+        nombre = mostrar_un_prod(prod_det_vent['id_producto'])['nombre']
+
+        p = {
+            prod_det_vent['id_producto']:{
+                'nombre': nombre,
+                'cantidad_vendidos': cant_prod_v
+            }
+        }
+        prod_vendidos.update(p)
+
+          
         forma_p = ""
         metodo = {}
         cont_metodo = 0
         importe_metodo = 0
         if venta['forma_pago'] in metodos_contados:
             forma_p = venta['forma_pago']
-            cont_metodo = int(metodos_contados[forma_p][1][0]) + 1
-            importe_metodo = int(metodos_contados[forma_p][1][1]) + venta['importe_total']
+            for clave, valor in metodos_contados[forma_p].items():
+                if clave == 'veces_usado':
+                    cont_metodo = valor + 1
+                elif clave == 'importe_metodo':
+                    importe_metodo = valor + venta['importe_total']
         else:
             forma_p = venta['forma_pago']
             metodo = {}
             cont_metodo = 1
             importe_metodo = venta['importe_total']
-        metodo.update({cont_metodo:importe_metodo})
+        
+        metodo.update({'veces_usado':cont_metodo})
+        metodo.update({'importe_metodo':importe_metodo})
 
         metodos_contados.update({venta['forma_pago']:metodo})
 
+    prod_menos_vendido = []
+    prod_mas_vendido = []
+    prod_menos_vendido = sorted(prod_vendidos.items(), key=lambda prod: prod[1]['cantidad_vendidos'])
+    prod_mas_vendido = sorted(prod_vendidos.items(), key=lambda prod: prod[1]['cantidad_vendidos'], reverse=True)
+    # print(prod_menos_vendido)
+    # print(prod_mas_vendido)
 
-    prod_mas_vendido = sorted(prod_vendidos.items(), key=lambda prod: prod[1])
-    prod_menos_vendido = sorted(prod_mas_vendido, reverse=True)
 
-    metodo_mas_usado = sorted(metodos_contados.items(), key=lambda metodo: list(metodo[1])[0])
-    metodo_menos_usado = sorted(metodo_mas_usado, reverse=True)
+    metodo_menos_usado = []
+    metodo_mas_usado = []
+    metodo_menos_usado = sorted(metodos_contados.items(), key=lambda metodo: metodo[1]['veces_usado'])
+    metodo_mas_usado = sorted(metodos_contados.items(), key=lambda metodo: metodo[1]['veces_usado'], reverse=True)
+    # print(metodo_menos_usado)
+    # print(metodo_mas_usado)
+
+    metodo_mas_recaudado= []
+    metodo_menos_recaudado= []
+    metodo_mas_recaudado= sorted(metodos_contados.items(), key=lambda metodo: metodo[1]['importe_metodo'])
+    metodo_menos_recaudado = sorted(metodos_contados.items(), key=lambda metodo: metodo[1]['importe_metodo'], reverse=True)
+    # print(metodo_menos_recaudado)
+    # print(metodo_mas_recaudado)
 
 
     # print('|-Cantidad de ventas: 99 - $999999')
@@ -389,29 +521,40 @@ def estadisticas_ventas():
     info_ventas.update({'metodo_mas_usado':metodo_mas_usado[0]})
     # print('|-Metodo de Pago menos usado: tarjeta - $1000')
     info_ventas.update({'metodo_menos_usado':metodo_menos_usado[0]})
-    
+
+
+    # print('|-Metodo de Pago que mas recaudo: efectivo - $99999')
+    info_ventas.update({'metodo_mas_recaudado':metodo_mas_recaudado[0]})
+    # print('|-Metodo de Pago que menos recaudo: tarjeta - $1000')
+    info_ventas.update({'metodo_menos_recaudado':metodo_menos_recaudado[0]})
+
     return info_ventas
 
 
 def estadisticas_generales():
     limpiar_consola()
     print('|###--··  TecStore  ··--###|')
-    print('|##·· Estadisticas de Productos')
     stats_prod=estadisticas_productos()
-    print(f'|-Cantidad de Productos: {len(productos)} ')
+    stats_vent=estadisticas_ventas()
+    print('|##·· Estadisticas Generales')
+    print(f'|-Cantidad de productos registrados: {len(productos)}')
+    print(f'|-Cantidad de Ventas Totales: {stats_vent['cantidad_venta']} - Recaudado: ${stats_vent['cantidad_ganancia']}')
+    print('|')
+    print('|##·· Estadisticas Productos')
+    print(f'|-Producto Mas Vendido: {stats_vent['prod_mas_vendidos'][1]['nombre']} - vendidos: {stats_vent["prod_mas_vendidos"][1]['cantidad_vendidos']}')
+    print(f'|-Producto Menos Vendido: {stats_vent['prod_menos_vendidos'][1]['nombre']} - vendidos: {stats_vent["prod_menos_vendidos"][1]['cantidad_vendidos']}')
+    print('|')
+    print('|##·· Estadisticas Stock')
     print(f'|-Cantidad total de Productos: {stats_prod['cantidad_existencias']} - capital: $ {stats_prod['capital_neto']}')
     print(f'|-Producto con mas Stock: {stats_prod['prod_stock_bajo']['nombre']} - stock: {stats_prod['prod_stock_bajo']['stock']}')
     print(f'|-Producto con menos Stock: {stats_prod['prod_stock_alto']['nombre']} - stock: {stats_prod['prod_stock_alto']['stock']}')
-    
+    print('|')
+    print('|##·· Estadisticas Metodos de Pago')
+    print(f'|-Mas usado: {stats_vent['metodo_mas_usado'][0]} ({stats_vent['metodo_mas_usado'][1]['veces_usado']} veces) - ${stats_vent['metodo_mas_usado'][1]['importe_metodo']}')
+    print(f'|-Menos usado: {stats_vent['metodo_menos_usado'][0]} ({stats_vent['metodo_menos_usado'][1]['veces_usado']} veces) - ${stats_vent['metodo_menos_usado'][1]['importe_metodo']}')
+    print(f'|-Metodo que mas recaudo: {stats_vent['metodo_mas_recaudado'][0]} - ${stats_vent['metodo_mas_recaudado'][1]['importe_metodo']}')
+    print(f'|-Metodo que menos recaudo: {stats_vent['metodo_menos_recaudado'][0]} - ${stats_vent['metodo_menos_recaudado'][1]['importe_metodo']}')
     print('|##················································')
-    print('|##·· Estadisticas de Ventas')
-    stats_vent=estadisticas_ventas()
-    print(f'|-Cantidad de ventas: {stats_vent['cantidad_venta']} - ${stats_vent['cantidad_ganancia']}')
-    print(f'|-Producto Mas Vendido: {mostrar_un_prod(int(stats_vent['prod_mas_vendidos'][0]))['nombre']} - ventas: {stats_vent["prod_mas_vendidos"][1]}')
-    print(f'|-Producto Menos Vendido: {mostrar_un_prod(int(stats_vent['prod_menos_vendidos'][0]))['nombre']} - ventas: {stats_vent["prod_menos_vendidos"][1]}')
-    print(f'|-Metodo de Pago mas usado: {stats_vent['metodo_mas_usado'][0]} - ${stats_vent['metodo_mas_usado'][1][1]}')
-    print(f'|-Metodo de Pago menos usado: {stats_vent['metodo_menos_usado'][0]} - ${stats_vent['metodo_menos_usado'][1][1]}')
-    print(f'|##················································')
 
 
 while True:
